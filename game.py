2aa6c8a25df18286f960e965215634c2bc437378
dave89gray <david_gray89@aol.com>
2022-08-07 13:53:21 +0000

    Add welcome file and start of game.py code

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+class User:
+    """
+    Base player class for the player
+    """
+    def __init__(self, user):
---
+++ welcome.py
@@ -0,0 +1,22 @@
+#class Welcome:
+    #def welcome(self):
+        #"""
+        #This function shall print the welcome
+        #message and the ASCII game board
+        #"""
+        #User_name = "\n Please enter your name: \n"
+        #welcome_ascii = """
+#                      |          |
+#                      |          |
+#                      |          |
+#             _________|__________|__________
+#                      |          |
+#                      |          |
+#                      |          |
+#                      |          |
+#             _________|__________|__________
+#                      |          |
+#                      |          |
+#                      |          |
+#        """
+#        print("Welcome to Knoughts and Crosses")
