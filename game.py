5de31ea8093e7fce0f0b3772f39910344f393898
dave89gray <david_gray89@aol.com>
2022-08-07 14:01:45 +0000

    Change the instance to player (general for game) instead of user

--- game.py
+++ game.py
@@ -1,6 +1,11 @@
 
 class User:
     """
-    Base player class for the player
+    Base player class for the player of the game
     """
-    def __init__(self, user):
+    def __init__(self, player):
+        """
+        This function represents which player is playing
+        """
+        # Set the current instance of player (user or computer)
+        self.player = player
