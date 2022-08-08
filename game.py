9abaf4134981b9711572d7f5eef1ed420599a2a4
dave89gray <david_gray89@aol.com>
2022-08-08 17:31:14 +0000

    Add function so the board will show in the command line

--- game.py
+++ game.py
@@ -4,4 +4,18 @@ class Game:
     Main class for the game to be played
     """
     def __init__(self):
-        self.board
+        """
+        Function to create the game board
+        """
+        # Create the list with length 9 that represents
+        # a 3x3 game board
+        self.board = ['  ' for _ in range(9)]
+
+        # Variable to keep track of who the current winner is
+        self.current_winner = None
+
+    def show_board(self):
+        """
+        Function that will show the board on
+        the command line
+        """
