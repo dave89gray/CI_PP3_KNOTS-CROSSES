a76ac2df9f81c46edf1e7a8bf47917ea8cc8abef
dave89gray <david_gray89@aol.com>
2022-08-08 17:09:30 +0000

    Change player functions to the player.py file

--- players.py
+++ players.py
@@ -1,1 +1,42 @@
 
+class Player:
+    """
+    Base player class for the player of the game which
+    the user or computer will be be derived from
+    """
+    def __init__(self, player):
+        """
+        This function represents which player is playing
+        """
+        # Set the current instance of player (user or computer)
+        self.player = player
+
+        def get_next_move(self, game):
+            """
+            This function ensures each player gets a next
+            move if its available
+            """
+            pass
+
+class User(Player):
+    """
+    User to inherit the base Player function and develop it
+    further into the user
+    """  
+    def __init__(self, player):
+        """
+        Function to create instance of user while using
+        super() to inherit player attributes
+        """
+        super().__init__(player)
+
+    def get_next_move(self, game):
+
+class computer(player):
+        """
+        Function to create instance of user while using
+        super() to inherit player attributes
+        """
+        super().__init__(player)
+
+        def get_next_move(self, game):
